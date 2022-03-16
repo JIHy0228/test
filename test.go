aa2c768c63907aeaaf4143c2bdb04b25a5498b53
Sangmu Yun <sangmu811@naver.com>
2022-03-16 14:25:36 +0900

    :sparkles: test
    testcommit

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var a int = 5
+
+func main() {
+	fmt.Println("hello world")
+
+	var age int = 26
+	var name string = "매튜"
+
+	fmt.Printf("저의 이름은 %s이고 나이는 %d입니다\n", name, age)
+
+	ten := 10
+	fmt.Print(ten)
+}
